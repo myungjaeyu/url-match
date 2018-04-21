1ad6da134bc72b096a626dd2841a59c0a09c6c40
u4bi <admin@naver.id>
2018-04-21 13:58:53 +0900

    changed the type of the retrun value of function.

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,34 @@
 extern crate regex;
-use regex::Captures;
+use std::collections::HashMap;
 
 mod util;
 
-pub fn matcher<'a>(pattern: &'a str, url: &'a str) -> Result<Captures<'a>, bool> {
+pub fn matcher<'a>(pattern: &'a str, url: &'a str) -> Option<HashMap<String, &'a str>> {
+
+    let mut map = HashMap::new();
 
     let expr = util::expression(pattern);
     let re = util::regexp(&expr);
 
     if re.is_match(url) {
-        Ok(re.captures(url).unwrap())
+
+        let caps = re.captures(url).unwrap();
+
+        for (index, key) in re.capture_names().enumerate() {
+
+            if key.is_some() {
+                let k = String::from(key.unwrap());
+                let v = caps.get(index).unwrap().as_str();
+
+                map.insert(k, v);
+
+            }
+
+        }
+        
+        Some(map)
     } else {
-        Err(false)
+        None
     }
 
 }
