a9343369cc5ac3fbd41eb4e6fdc024a6ce1525f3
Eunchong Yu <gracie@spoqa.com>
2018-04-25 05:27:39 +0900

    Reuse the compiled regexes

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+#[macro_use]
+extern crate lazy_static;
+
 extern crate regex;
+
 use std::collections::HashMap;
 
 mod util;
--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,23 @@
 use regex::Regex;
 
 pub fn escape(pattern : &str) -> String {
+    lazy_static! {
+        static ref KEY: Regex = Regex::new(r"(?P<key>[?&.])").unwrap();
+    }
 
-    let exp = String::from(Regex::new(r"(?P<key>[?&.])").unwrap().replace_all(pattern, r"\$key"));
+    let exp = String::from(KEY.replace_all(pattern, r"\$key"));
 
     exp
 }
 
 pub fn expression(pattern : &str) -> String {
-
-    let chars = Regex::new(r":(?P<key>[a-zA-Z0-9_-]+)").unwrap();
+    lazy_static! {
+        static ref CHARS: Regex = Regex::new(r":(?P<key>[a-zA-Z0-9_-]+)").unwrap();
+    }
 
     let mut exp = String::from(r"^");
 
-    exp.push_str(&chars.replace_all( &escape(pattern), r"(?P<$key>[a-zA-Z0-9_-]+)"));
+    exp.push_str(&CHARS.replace_all( &escape(pattern), r"(?P<$key>[a-zA-Z0-9_-]+)"));
     exp.push_str("$");
     
     exp
