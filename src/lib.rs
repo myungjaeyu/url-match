933589db04a77ee2fb2f908462b93ac0ffbdfabc
Eunchong Yu <gracie@spoqa.com>
2018-04-25 05:43:05 +0900

    Reduce unnecessary match operation

--- src/lib.rs
+++ src/lib.rs
@@ -14,25 +14,14 @@ pub fn matcher<'a>(pattern: &'a str, url: &'a str) -> Option<HashMap<String, &'a
     let expr = util::expression(pattern);
     let re = util::regexp(&expr);
 
-    if re.is_match(url) {
+    let caps = re.captures(url)?;
 
-        let caps = re.captures(url).unwrap();
-
-        for (index, key) in re.capture_names().enumerate() {
-
-            if key.is_some() {
-                let k = String::from(key.unwrap());
-                let v = caps.get(index).unwrap().as_str();
-
-                map.insert(k, v);
-
-            }
-
+    for (index, key) in re.capture_names().enumerate() {
+        if let (Some(k), Some(c)) = (key, caps.get(index)) {
+            map.insert(k.to_owned(), c.as_str());
         }
-        
-        Some(map)
-    } else {
-        None
     }
+
+    Some(map)
 
 }
